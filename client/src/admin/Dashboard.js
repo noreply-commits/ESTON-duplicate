9b07b452976568510a1cde05e42cae7d0442f5c3
noreply-commits <noreply@eston.edu.gh>
2025-09-06 19:44:03 +0000

    Update Dashboard.js

--- client/src/admin/Dashboard.js
+++ client/src/admin/Dashboard.js
@@ -100,10 +100,11 @@ const AdminDashboard = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-end mb-4">
           <button
-            onClick={() => {
-              logout();
-              navigate('/login');
-            }}
+onClick={() => {
+  logout(); // Call your logout function
+  window.location.href = 'https://estonschool.onrender.com/';
+}}
+
             className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
           >
             Logout
