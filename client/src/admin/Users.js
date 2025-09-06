082a4e1e56b33fe7638ca6bab9da5da897c1623d
noreply-commits <noreply@eston.edu.gh>
2025-09-06 20:48:43 +0000

    Update Users.js

--- client/src/admin/Users.js
+++ client/src/admin/Users.js
@@ -252,7 +252,7 @@ const AdminUsers = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="min-h-screen bg-gray-50">
       <AdminNavbar />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
