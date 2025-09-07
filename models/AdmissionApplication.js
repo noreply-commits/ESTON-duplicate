26be2d71261477ed1b05f45f3bda11b51586e79c
noreply-commits <noreply-commits@users.noreply.github.com>
2025-09-07 13:30:18 +0000

    update

--- models/AdmissionApplication.js
+++ models/AdmissionApplication.js
@@ -41,7 +41,7 @@ class AdmissionApplication {
         reason_for_course,
         how_hear,
         declaration
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *`,
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) RETURNING *`,
       [
         firstName,
         middleName,
